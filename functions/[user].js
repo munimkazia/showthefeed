e67651a9279f4cb17afaa486f6b606fcf9fddf42
munim <munim2020@gmail.com>
2026-06-04 10:55:21 +0100

    sort

--- functions/[user].js
+++ functions/[user].js
@@ -2,17 +2,27 @@ export async function onRequestGet(context) {
   const { params, request } = context;
   const username = params.user;
   const url = new URL(request.url);
-  const isRecent = url.searchParams.get('recent') === 'true';
+  
+  // Default to a 24-hour window if no specific param is provided,
+  // or allow the user to specify a broader window.
+  const isRecent = url.searchParams.get('recent') !== 'false';
 
   if (!username || username === 'favicon.ico') {
-    return next();
+    return context.next();
   }
 
-  // Target: Google search for tweets from the specific user
+  // searchQueue: site:x.com/username ensures we only get that account.
+  // We use x.com instead of twitter.com as x.com is the current primary domain.
   let searchQueue = `site:x.com/${username}`;
   
-  // Google parameter for "past 24 hours" is &tbs=qdr:d
-  const timeParam = isRecent ? '&tbs=qdr:d' : '';
+  // Google Time Parameters (tbs):
+  // qdr:h (past hour)
+  // qdr:d (past 24 hours)
+  // qdr:w (past week)
+  // sbd:1 (sort by date) - Note: sbd:1 often requires a qdr parameter to be present.
+  
+  // We use past 24 hours (qdr:d) and sort by date (sbd:1) for the best "feed" experience.
+  const timeParam = isRecent ? '&tbs=qdr:d,sbd:1' : '';
 
   const redirectUrl = `https://www.google.com/search?q=${encodeURIComponent(searchQueue)}${timeParam}`;
 
