5fe000ea527cbe973b51899d82a385dd0b8f8128
munim <munim2020@gmail.com>
2026-06-04 11:04:46 +0100

    Default to last 7 days with sorting; add attribution and campaign links

--- functions/[user].js
+++ functions/[user].js
@@ -3,26 +3,19 @@ export async function onRequestGet(context) {
   const username = params.user;
   const url = new URL(request.url);
   
-  // Default to a 24-hour window if no specific param is provided,
-  // or allow the user to specify a broader window.
+  // Default to a 1-week window if no specific param is provided.
   const isRecent = url.searchParams.get('recent') !== 'false';
 
   if (!username || username === 'favicon.ico') {
     return context.next();
   }
 
-  // searchQueue: site:x.com/username ensures we only get that account.
-  // We use x.com instead of twitter.com as x.com is the current primary domain.
+  // site:x.com/username ensures we only get that account.
   let searchQueue = `site:x.com/${username}`;
   
-  // Google Time Parameters (tbs):
-  // qdr:h (past hour)
-  // qdr:d (past 24 hours)
-  // qdr:w (past week)
-  // sbd:1 (sort by date) - Note: sbd:1 often requires a qdr parameter to be present.
-  
-  // We use past 24 hours (qdr:d) and sort by date (sbd:1) for the best "feed" experience.
-  const timeParam = isRecent ? '&tbs=qdr:d,sbd:1' : '';
+  // We use past week (qdr:w) and sort by date (sbd:1) as the default.
+  // This ensures results are visible even if there hasn't been a post today.
+  const timeParam = isRecent ? '&tbs=qdr:w,sbd:1' : '';
 
   const redirectUrl = `https://www.google.com/search?q=${encodeURIComponent(searchQueue)}${timeParam}`;
 
